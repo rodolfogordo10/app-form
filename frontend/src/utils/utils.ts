5a7699d5b8ac78886c64afb9a76cc4f328ee7293
Rodolfo Mota Martins <rodolfo.martins@ativy.local>
2023-10-02 04:13:11 -0300

    Ajustes no package

--- frontend/src/utils/utils.ts
+++ frontend/src/utils/utils.ts
@@ -1,10 +1,3 @@
-export const stringAvatar = (name: string) => {
-  return {
-    size: 28,
-    children: `${name.split(' ')[0][0]}`,
-  };
-};
-
 export const notMask = (query: string) => {
   return query.replace(/[^0-9]+/g, '');
 };
@@ -27,51 +20,3 @@ export const convertHexToRGB = (hex: string) => {
     return [(c >> 16) & 255, (c >> 8) & 255, c & 255].join(',');
   }
 };
-
-export const renderProductCount = (
-  page: number,
-  productPerPage: number,
-  totalProductLenth: number,
-) => {
-  const startNumber = page * productPerPage;
-  let endNumber = (page + 1) * productPerPage;
-
-  if (endNumber > totalProductLenth) endNumber = totalProductLenth;
-
-  return `Showing ${
-    startNumber + 1
-  }-${endNumber} of ${totalProductLenth} products`;
-};
-
-/**
- * CALCULATE PRICE WITH PRODUCT DISCOUNT THEN RETURN NEW PRODUCT PRICES
- * @param  price - PRODUCT PRICE
- * @param  discount - DISCOUNT PERCENT
- * @returns - RETURN NEW PRICE
- */
-
-export function calculateDiscount(price: number, discount: number) {
-  const afterDiscount = Number((price - price * (discount / 100)).toFixed(2));
-  return currency(afterDiscount);
-}
-
-/**
- * CHANGE THE CURRENCY FORMAT
- * @param  price - PRODUCT PRICE
- * @param  fraction - HOW MANY FRACTION WANT TO SHOW
- * @returns - RETURN PRICE WITH CURRENCY
- */
-
-export function currency(price: number, fraction: number = 2) {
-  // const { publicRuntimeConfig } = getConfig();
-  // currency: publicRuntimeConfig.currency,
-
-  const formatCurrency = new Intl.NumberFormat(undefined, {
-    currency: 'BRL',
-    style: 'currency',
-    maximumFractionDigits: fraction,
-    minimumFractionDigits: fraction,
-  });
-
-  return formatCurrency.format(price);
-}
